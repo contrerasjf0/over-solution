ca67874735c9caf73237b91f4dfddfb7c86f1ed1
contrerasjf <contrerasjf.0@hotmail.com>
2019-06-03 05:58:26 -0500

    feat(register): Implement validation with joi
    Resolve: C-10

---
+++ routes.js
@@ -0,0 +1,40 @@
+onst Joi = require('joi')
+const site = require('./controllers/site')
+const user = require('./controllers/user')
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: site.home
+  },
+  {
+    method: 'GET',
+    path: '/register',
+    handler: site.register
+  },
+  {
+    method: 'POST',
+    options: {
+      validate: {
+        payload: {
+          name: Joi.string().required().min(3),
+          email: Joi.string().email().required(),
+          password: Joi.string().required().min(6)
+        }
+      }
+    },
+    path: '/create-user',
+    handler: user.createUser
+  },
+  {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: '.',
+        index: ['index.html']
+      }
+    }
+  }
+]
